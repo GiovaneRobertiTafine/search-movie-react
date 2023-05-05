cab71b193b02e213b94aebcfca362cbff58136e7
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2023-05-05 16:12:11 -0300

    Corrigindo scroll modal

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -45,9 +45,14 @@ function ModalComponent({ closeModal }: Modal) {
         getCredits();
         getFaixaEtaria();
         window.onclick = e => {
+            console.log((e.target as HTMLElement)?.id === "back-drop");
+            console.log((e.target as HTMLElement)?.id === "modal-header");
             if ((e.target as HTMLElement).id === "back-drop" || (e.target as HTMLElement).id === "modal-header") {
                 closeModal(true);
                 document.documentElement.style.overflow = 'auto';
+            } else {
+                document.documentElement.style.overflow = 'hidden';
+
             }
         };
     }, [filme]);
